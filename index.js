7f13030a6b41dc7f361455a8d878e09a38e6a2e9
Muhammad Ahmad <76677577+tech-ahmad@users.noreply.github.com>
2023-02-23 11:23:01 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const heading = document.getElementById("h1");
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', () => {
+  heading.innerHTML = "This is heading and what is your problem";
+})
